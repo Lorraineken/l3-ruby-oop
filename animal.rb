8c80a71085960f623dfda9ce742336be5f1bda52
Lorraineken <lorrainekupa@gmail.com>
2023-02-13 12:12:52 +0300

    Created Animal class

--- animal.rb
+++ animal.rb
@@ -1,3 +1,31 @@
 # TODO: CREATE CLASS AND INSTANCE METHODS (INCLUDE VARIABLES)
+class Animal
+
+    def initialize(name)
+      puts "#{name} has  been created"
+    end
+
+  def play_sound
+    puts "playing sound"
+  end
+
+  def move 
+    play_sound
+    puts "Moving"
+  end
+
+end
+
+zebra=Animal.new('zebra')
+giraffe=Animal.new('giraffe')
+
+pp "zebra #{zebra}"
+pp "giraffe #{giraffe}"
+
+zebra.play_sound
+giraffe.move
 
 # TODO: Use self to access instance variables
+
+
+
